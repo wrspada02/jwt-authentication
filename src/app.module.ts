557d8b2c348d07565a70a4a6f398db4dc5491027
William Spada <williamrspada02@gmail.com>
2024-08-30 15:42:06 -0300

    refactor: remove unecessary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService, UsersService],
 })
 export class AppModule {}
